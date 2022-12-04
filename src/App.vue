36e210a7a6572a6c99fa85970b5ab9dd19f938f4
Flora Preuvot <flora@qomon.com>
2022-12-04 17:48:15 +0100

    add generate jokes feature

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class="w-full h-full flex justify-center items-center flex-col gap-y-4">
+    <div class="text-3xl">{{ data.setup }}</div>
+    <button @click="showDelivery = true" role="button" name="Tell Me!">Tell Me!</button>
+
+    <div v-if="showDelivery" class="text-3xl">{{ data.delivery }}</div>
+    <button v-if="showDelivery" @click="getJoke" role="button" name="Another">Another</button>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+
+let data = ref({ setup: '', delivery: '' })
+let showDelivery = ref(false)
+
+const getJoke = async () => {
+  showDelivery.value = false
+
+  const res = await fetch('https://v2.jokeapi.dev/joke/christmas').catch(() => alert('An error has occured.'))
+  data.value = res ? await res.json() : null
+}
+
+getJoke()
 </script>
